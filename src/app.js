e6fc6ed266a7dbb661bebffe59078f9f0ae958a6
xynyx <matt.taylr22@gmail.com>
2020-06-04 10:29:04 -0400

    Change to API instead of hard-coded data; has imgs

--- src/app.js
+++ src/app.js
@@ -8,28 +8,25 @@ const searchClient = algoliasearch(
 );
 
 const index = searchClient.initIndex('mtg-search');
-const cards = require('../StandardPrintings.json');
-const ELDCards = cards[0].ELD.cards;
+// const cards = require('../StandardPrintings.json');
+// const ELDCards = cards[0].ELD.cards;
 
-console.log(ELDCards);
 
 index.setSettings({
   // Select the attributes you want to search in
-  searchableAttributes: ['colors', 'convertedManaCost', 'name', 'power'],
+  searchableAttributes: [
+    'colors',
+    'convertedManaCost',
+    'name',
+    'power',
+    'type',
+  ],
   customRanking: ['asc(name)'],
-  attributeForDistinct: "name",
+  attributeForDistinct: 'name',
   distinct: 1,
   // Set up some attributes to filter results on
-  attributesForFaceting: ['name', 'colors'],
+  // attributesForFaceting: ['name', 'colors'],
 });
-
-index
-  .saveObjects(ELDCards, {
-    autoGenerateObjectIDIfNotExist: true,
-  })
-  .then(({ objectIDs }) => {
-    // console.log(objectIDs);
-  });
 
 const search = instantsearch({
   indexName: 'mtg-search',
@@ -46,6 +43,49 @@ search.addWidgets([
   }),
 ]);
 
+// const encoded = encodeURIComponent("https://api.scryfall.com/cards/search?q=is:funny");
+
+Promise.all([
+  fetch(`https://api.scryfall.com/cards/search?q=is:funny`)
+    .then(card => {
+      // console.log('card :>> ', card);
+      return card.json();
+    })
+    .then(body => {
+      // console.log('body :>> ', body);
+      return body.data;
+    }),
+  fetch(
+    `https://api.scryfall.com/cards/search?format=json&include_extras=true&include_multilingual=false&order=name&page=2&q=is%3Afunny&unique=cards`
+  )
+    .then(card => {
+      // console.log('card2 :>> ', card);
+      return card.json();
+    })
+    .then(body => {
+      // console.log('body2 :>> ', body);
+      return body.data;
+    }),
+  fetch(
+    `https://api.scryfall.com/cards/search?format=json&include_extras=true&include_multilingual=false&order=name&page=3&q=is%3Afunny&unique=cards`
+  )
+    .then(card => {
+      // console.log('card2 :>> ', card);
+      return card.json();
+    })
+    .then(body => {
+      // console.log('body2 :>> ', body);
+      return body.data;
+    }),
+]).then(cards => {
+  const combinedCards = cards.flat();
+  console.log('cards :>> ', combinedCards);
+  return index.saveObjects(combinedCards, {
+    autoGenerateObjectIDIfNotExist: true,
+  })
+
+});
+
 search.addWidget(
   instantsearch.widgets.hits({
     container: '#hits',
@@ -58,7 +98,11 @@ search.addWidget(
   })
 );
 
-index.clearObjects()
-
+// index.clearObjects();
 
 search.start();
+
+/* art_crop */
+//
+
+/* Scryfall produces multiple sizes of images and image crops for each Card object. Links to these images are available in each Card objects’ image_uris properties. */
--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,28 @@
 export function hitTemplate(hit) {
+  const { color_identity, flavor_text, image_uris, mana_cost, name, oracle_text, rarity } = hit;
+  console.log('image_uris.art_crop :>> ', image_uris.art_crop);
+  console.log('mana_cost :>> ', mana_cost);
   return `
     <div class="hit">
-      <div class="hit-image">
-        <img />
-      </div>
       <div class="hit-content">
-        <div>
-          <div class="hit-name">${hit.name}</div>
-          <div class="hit-description">${hit.tcgplayerProductId}</div>
+        <div class="hit-title">
+          <h2 class="hit-name">${name}</h2>
+          <span class=${mana_cost? "hit-mana" : "invisible" }>${mana_cost}</span>
+          <div class="hit-type">${rarity}</div>
         </div>
-        <div class="hit-power">${hit.power}</div>
+        <div >
+          <img class="hit-image" src="${image_uris.art_crop}" />
+        </div>
+        <div class="hit-description">${oracle_text}</div>
+        <div class="hit-color">${color_identity}</div>
+        <span class=${hit.power? "hit-stats" : "invisible" }>${hit.power}/${hit.toughness}</span>
       </div>
     </div>
   `;
 }
+
+
+
+/*       // <div class="hit-image">
+      //   <img />
+      // </div> */

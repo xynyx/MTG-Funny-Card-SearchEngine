cb3a11ddf4b548f47f7d870b6f881e476778d1d3
xynyx <matt.taylr22@gmail.com>
2020-06-04 20:08:35 -0400

    new mana icons

--- src/helpers.js
+++ src/helpers.js
@@ -10,50 +10,55 @@ export function hitTemplate(hit) {
     type_line,
   } = hit;
 
-  let finalMana = '';
-  let colorArr = [];
   const manaWithoutBrackets = mana_cost.replace(/{|}/g, '');
-  // console.log('manaWithoutBrackets.slice(0, 1) :>> ', manaWithoutBrackets.slice(0, 1));
-  const number = manaWithoutBrackets.slice(0, 1);
-  const colors = manaWithoutBrackets.substring(1).split('');
-  colors.forEach(color => colorArr.push(color));
-  console.log('colors :>> ', colorArr);
-  // const formatManaNumber = () => {
-  //   // if (manaWithoutBrackets.length === 1) return finalMana = manaWithoutBrackets;
-  // };
 
-  // If first char is a letter, then there is no generic mana cost ->
-
-  const formatManaColors = () => {
-    manaWithoutBrackets.substring(1);
+  const converter = {
+    1: '[',
+    2: '\\',
+    3: ']',
+    4: '^',
+    5: '_',
+    6: '`',
+    7: '{',
+    8: '|',
+    9: '}',
+    0: '~',
+    R: '<',
+    G: '>',
+    B: '=',
+    U: '+',
+    W: '@',
   };
 
-  // console.log('formatMana :>> ', formatManaNumber());
-  // console.log('finalMana :>> ', finalMana);
-  // console.log('formatManaColors() :>> ', formatManaColors());
+  const convertedIcons = () => {
+    return manaWithoutBrackets.split("").map((letter => {
+     return letter = converter[letter];
+    })).join("");
+    
+  }
 
   return `
     <div class="hit">
       <div class="hit-content">
         <div class="hit-title">
           <h2 class="hit-name">${name}</h2>
-
-
           <div class="hit-type">${type_line}</div>
           <div class="img-container">
             <img class="hit-image" src="${image_uris.art_crop}" />
           </div>
            </div>
-           <div class="hit-rarity">${rarity.charAt(0).toUpperCase() + rarity.slice(1)}          <span class="hit-mana">${mana_cost}
-           </span></div>
+           <div class="hit-rarity">${rarity.charAt(0).toUpperCase() +
+             rarity.slice(1)}
+                <span class="hit-mana">${convertedIcons()}</span>
+           </div>
            <div class="hit-text">
-           <div class="hit-description">${oracle_text}</div>
+           <div class=${oracle_text? "hit-description" : "invisible"}>${oracle_text}</div>
            <div class=${
              flavor_text ? 'hit-flavor-text' : 'invisible'
            }>${flavor_text}</div>
            <span class=${hit.power ? 'hit-stats' : 'invisible'}>${hit.power}/${
-              hit.toughness
-              }</span>
+    hit.toughness
+  }</span>
            </div>
       </div>
     </div>
